e688ed80a850bcd1f6bff64b606d3fbdfdbb358f
Rumani Kafle <rkafle@uncc.edu>
2019-09-15 22:59:27 +0000

    RubyBasics 2 - Part 2 rspec enabled

--- RubyBasics2/spec/rubybasics2_spec.rb
+++ RubyBasics2/spec/rubybasics2_spec.rb
@@ -11,13 +11,13 @@ describe "#hello" do
 
   it "The hello method returns the correct string [30 points]" , points: 30 do
     expect(hello("Dan").class).to eq(String)
-    expect(hello("Dan")).to eq('Hello, Dan'), "Incorrect results for input: \"Dan\""
+    m expect(hello("Dan")).to eq('Hello, Dan'), "Incorrect results for input: \"Dan\""
     expect(hello("BILL")).to eq('Hello, BILL'), "Incorrect results for input: \"BILL\""
     expect(hello("Mr. Ilson")).to eq('Hello, Mr. Ilson'), "Incorrect results for input: \"Mr. Ilson\""
   end
 end
 
-describe "#starts_with_consonant?", :disabled => true do
+describe "#starts_with_consonant?" do
   it "should be defined" do
     expect { starts_with_consonant?("d") }.not_to raise_error()#::NoMethodError)
   end
